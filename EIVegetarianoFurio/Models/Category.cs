608656c60bb3e321ea96d1e700b4b6a02f2cd59d
ImanAbidi@acer5750g <imanabidi@gmail.com>
2020-01-01 19:37:57 +0330

    dish und Category Klasses hzfg

---
+++ EIVegetarianoFurio/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace EIVegetarianoFurio.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public virtual ICollection<Dish> Dishes { get; set; } = new HashSet<Dish>();
+    }
+}
